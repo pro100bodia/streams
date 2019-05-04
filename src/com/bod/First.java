22be8d476c3d55529c648b8e3234481ff877a143
Bodia <bohdanpukhalsky@ukr.net>
2019-05-04 13:59:54 +0300

    first task

---
+++ src/com/bod/First.java
@@ -0,0 +1,23 @@
+package com.bod;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class First {
+    public static void main(String[] args) {
+        String[] names = {"01. Dmytro",
+                          "02. Stepan",
+                          "03. Bohdan",
+                          "04. Daria",
+                          "447. Nikita"};
+
+
+
+        List<String> namesList = Arrays.asList(names);
+
+        namesList
+                .stream()
+                .filter((p) -> p.matches("^(\\d*[13579]+).+"))
+                .forEach(System.out::println);
+    }
+}
